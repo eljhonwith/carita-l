cb45ff80bb421aba1fcc5dc88f6ed0f8af50f361
eljhonwith <91215236+eljhonwith@users.noreply.github.com>
2021-09-29 12:30:18 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,60 @@
+if (true) {
+    basic.showString("si le  dises  algo incrible  se queda sin palabras")
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        . # # # .
+        . . . . .
+        `)
+} else {
+    basic.showString("si no le dises  estara felis")
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        # . . . #
+        . # # # .
+        . . . . .
+        `)
+}
+if (true) {
+    basic.showString("si le dises cosas nuevas el reacionara sin palabras ")
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        . # # # .
+        . . . . .
+        `)
+} else {
+    basic.showString("si no le dises felis")
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        # . . . #
+        . # # # .
+        . . . . .
+        `)
+}
+if (true) {
+    basic.showString("decirle como  sale una nueva planta ")
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        . # # # .
+        . . . . .
+        `)
+} else {
+    basic.showString("si no   le dises felis  ")
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        # . . . #
+        . # # # .
+        . . . . .
+        `)
+}
+basic.forever(function () {
+	
+})
